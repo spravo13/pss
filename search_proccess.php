c2755188e36f96859410d92dd9c15806f48bc4b2
Spencer Bravo <spencer@codeshrub.com>
2015-07-10 00:04:23 -0700

    Add base files for program

---
+++ search_proccess.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * PHP Site Search
+ *
+ * PHP version 5.5
+ *
+ * @category File
+ * @package  PSS
+ * @author   Spencer Bravo <spencer@codeshrub.com>
+ * @license  https://www.gnu.org/licenses/gpl.html GNU GENERAL PUBLIC LICENSE
+ * @link     https://github.com/spravo13/pss
+ */
+
+if ($_POST['submit'] == "Search!") {
+
+
+}
+
+else {
+        $_SESSION['alert'] = "<div class=\"alert alert-danger\"><strong>Oh snap!</strong> Something went wrong... check the input and try again.</div>";
+}
+
+?>
