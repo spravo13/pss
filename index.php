2d7031f4c426ad16bd932a6f1e2712a72de5d059
Spencer Bravo <spencer@codeshrub.com>
2015-07-10 00:34:28 -0700

    Clean up organization. Make all files into one

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php
+/**
+ * PHP Site Search
+ *
+ * PHP version 5.5
+ *
+ * @category File
+ * @package  PSS
+ * @author   Spencer Bravo <spencer@codeshrub.com>
+ * @license  https://www.gnu.org/licenses/gpl.html GNU GENERAL PUBLIC LICENSE
+ * @link     https://github.com/spravo13/pss
+ */
+
+if ($_POST['submit'] == "Search!") {
+        echo "hey";
+
+} else {
+        $_SESSION['alert'] = "<div class=\"alert alert-danger\"><strong>Oh snap!</strong> Something went wrong... check the input and try again.</div>";
+}
+
+?>
+
 <!DOCTYPE html>
   <head>
     <title>Search</title>
