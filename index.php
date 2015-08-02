8804d1f19b70a7d063caf818436aeec29725af9f
Spencer Bravo <spencer@codeshrub.com>
2015-08-01 23:03:52 -0700

    Set basic file up to prepare for search proccess

--- index.php
+++ index.php
@@ -12,32 +12,36 @@
  */
 
 if ($_POST['submit'] == "Search!") {
+        
+} else {
+        
+        ?>
 
+        <!DOCTYPE html>
+          <head>
+            <title>Search</title>
+          </head>
+          <body>
+            <form action="index.php" method="post">
+              <p>
+                <label>search</label>
+                <input name="query" required="required" placeholder="Monty Python" type="text">
+              </p>
+              <p>
+                <label>url</label>
+                <input name="url" required="required" placeholder="www.codeshrub.com" type="text">
+              </p>
+              <p>
+                <label>max results</label>
+                <input name="max_results" placeholder="10" type="number">
+              </p>
+              <p>
+                <input name="submit" value="Search!" type="submit" id="submit">
+              </p>
+            </form>
+          </body>
+        </html>
+
+<?php
 }
-
 ?>
-
-<!DOCTYPE html>
-  <head>
-    <title>Search</title>
-  </head>
-  <body>
-    <form action="index.php" method="post">
-      <p>
-        <label>search</label>
-        <input name="query" required="required" placeholder="Monty Python" type="text">
-      </p>
-      <p>
-        <label>url</label>
-        <input name="url" required="required" placeholder="www.codeshrub.com" type="text">
-      </p>
-      <p>
-        <label>max results</label>
-        <input name="max_results" placeholder="10" type="number">
-      </p>
-      <p>
-        <input value="Search!" type="submit" id="submit">
-      </p>
-    </form>
-  </body>
-</html>
